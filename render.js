8880e9e165ec8cf467c8a8c43e4570a00c9432c9
me1a <mxh4373@gmail.com>
2020-01-10 00:28:33 +0800

    handle event and attribute in render

--- render.js
+++ render.js
@@ -1,6 +1,22 @@
 function render(element, parentDom) {
     const { type, props } = element;
     const dom = document.createElement(type)
+
+
+    const isListener = name => name.startsWith('on')
+
+    Object.keys(props).filter(isListener).forEach(item => {
+        const eventType = item.toLowerCase().substring(2)
+        dom.addEventListener(eventType, props.item)
+    })
+
+    const isAttribute = name => !isListener(name) && name !== 'children'
+
+    Object.keys(props).filter(isAttribute).forEach(item => {
+        dom[item] = props[item]
+    })
+
+
     const childElements = props.children || []
     childElements.forEach(item => {
         render(item, dom)
