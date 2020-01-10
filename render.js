bf7c612a365804f5af855f03e53df162b5255fc7
me1a <mxh4373@gmail.com>
2020-01-10 12:55:19 +0800

    rewrite render

--- render.js
+++ render.js
@@ -1,46 +1,7 @@
-
-// const textElement = {
-//     type: "span",
-//     props: {
-//       children: [
-//         {
-//           type: "TEXT ELEMENT", // 1
-//           props: { nodeValue: "Foo" } // 2
-//         }
-//       ]
-//     }
-//   };
-
-
-
-function render(element, parentDom) {
-    const { type, props } = element;
-
-
-    const isTextElement = type === 'TEXT ELEMENT'
-    const dom = isTextElement ? document.createTextNode('') : document.createElement(type)
-
-
-
-
-
-    const isListener = name => name.startsWith('on')
-
-    Object.keys(props).filter(isListener).forEach(item => {
-        const eventType = item.toLowerCase().substring(2)
-        dom.addEventListener(eventType, props.item)
-    })
-
-    const isAttribute = name => !isListener(name) && name !== 'children'
-
-    Object.keys(props).filter(isAttribute).forEach(item => {
-        dom[item] = props[item]
-    })
-
-
-    const childElements = props.children || []
-    childElements.forEach(item => {
-        render(item, dom)
-    })
-    parentDom.appendChild(dom)
+let rootInstance = null
+function render(element, container) {
+    const prevInstance = rootInstance
+    const nextInstance = reconcile(container, prevInstance, element)
+    rootInstance = nextInstance
 }
+
