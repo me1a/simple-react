caddb698613e98e8bb6b7e610e8b1e0f3b669cb6
me1a <mxh4373@gmail.com>
2020-01-10 12:50:07 +0800

    update dom

---
+++ reconcile.js
@@ -0,0 +1,11 @@
+function reconcile(parentDom, instance, element) {
+    if (instance === null) {
+        const newInstance = instantiate(element)
+        parentDom.appendChild(newInstance)
+        return newInstance
+    } else {
+        const newInstance = instantiate(element)
+        parentDom.replaceChild(newInstance.dom, instance.dom)
+        return newInstance
+    }
+}
