802782e743040ec2e89ecad968a3e6201a529d58
me1a <mxh4373@gmail.com>
2020-01-10 13:40:02 +0800

    running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import render from './render'
 
 
+render({
+    type: 'h1',
+    props: {
+        children: [
+            { type: 'TEXT ELEMENT', props: { nodeValue: 'hello react!' } }
+        ]
+    }
+}, document.querySelector('#app'))
