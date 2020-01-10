efbd87039129540f5b05242ac2f0aae0f68a887f
me1a <mxh4373@gmail.com>
2020-01-10 16:14:02 +0800

    render !

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@
 import createElement from './createElement'
 import render from './render'
 
-const app = (<div>
-    <h1 style='height: 100px'>hello react</h1>
-    <p>build you own react</p>
-</div>)
-console.log(app)
+const arr = [
+    { name: 'john', money: 9 },
+    { name: 'lisa', money: 12 },
+    { name: 'jack', money: 43 },
+]
 
-render(app, document.querySelector('#app'))
+function App() {
+
+    return <div>
+        {
+            arr.map(item => <div>
+                <h4 >{item.name} has $ <span>{item.money}</span>. <button onClick={() => {
+                    item.money = item.money + 1
+                    render(App(), document.querySelector('#app'))
+                }}>add</button></h4>
+            </div>)
+        }
+    </div>
+}
+
+
+render(App(), document.querySelector('#app'))
