ddf65d54cab034f0f6cc9232bd13d97f757480e8
me1a <mxh4373@gmail.com>
2020-01-10 13:59:23 +0800

    simple element diff

--- src/reconcile.js
+++ src/reconcile.js
@@ -1,10 +1,13 @@
 import instantiate from './instantiate'
+import updateDomProperties from './updateDomProperties'
 
 function reconcile(parentDom, instance, element) {
     if (instance === null) {
         const newInstance = instantiate(element)
         parentDom.appendChild(newInstance.dom)
         return newInstance
+    } else if (instance.element.type === element.types) {
+        updateDomProperties(instance.dom, instance.element.props, element.props)
     } else {
         const newInstance = instantiate(element)
         parentDom.replaceChild(newInstance.dom, instance.dom)
