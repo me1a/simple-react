49463d53cb9c8118caec1d3908b7cc3be9f7d0b9
me1a <mxh4373@gmail.com>
2020-01-30 00:04:37 +0800

    update property

--- src/diff.js
+++ src/diff.js
@@ -1,10 +1,15 @@
 import instanceCreate from './instance'
+import updateDomProperties from './updateDomProperties'
 
 function reconcile(parentDom, instance, element) {
     if (instance === null) {
         const newInstance = instanceCreate(element)
         parentDom.appendChild(newInstance.dom)
         return newInstance
+    } else if (instance.element.type === element.type) {
+        updateDomProperties(instance.dom, instance.element.props, element.props)
+        instance.element = element
+        return instance
     } else {
         const newInstance = instanceCreate(element)
         parentDom.replaceChild(newInstance.dom, instance.dom)
--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@
 import createElement from './createElement'
 import render from './render'
 
+let i = 0
 
-const app = <div>
-    <h1>app</h1>
-    <h1>app</h1>
+let app = <div>
+    <h1 onClick={click}>app</h1>
+    <h1>app{i}</h1>
 </div>
+
+function click() {
+    i++
+    console.log(i)
+    app = <div title={i}>
+        <h1 onClick={click}>app</h1>
+        <h1 title={i}>app</h1>
+    </div>
+    render(app, document.querySelector('#app'))
+}
 
 console.log(app)
 
 
 render(app, document.querySelector('#app'))
 
+// setInterval(() => {
+//     i++
+
+// }, 1000)
