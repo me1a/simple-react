236c80cfcd70650387957adceeb0de914d07cb3e
me1a <mxh4373@gmail.com>
2020-01-29 12:52:51 +0800

    add props to real node

--- src/render.js
+++ src/render.js
@@ -2,8 +2,24 @@ function render(vNode, parentDom) {
 
     const { type, props } = vNode
     const dom = document.createElement(type)
-    // 递归将 vNode 转化为真实dom
-    const children = props.children || []
-    children.forEach(child => render(child, dom))
+
+
+    const isListener = name => name.startsWith('on')
+    const isAttribute = name => !isListener(name) && name != "children"
+    Object.keys(props).forEach(prop => {
+        if (isListener(prop)) {
+            const eventType = prop.toLowerCase().substring(2)
+            dom.addEventListener(eventType, props[prop])
+        } else if (isAttribute) {
+            dom[prop] = props[prop]
+        } else {
+            // 递归将 vNode 转化为真实dom
+            const children = props.children || []
+            children.forEach(child => render(child, dom))
+        }
+    })
+
+
+
     parentDom.appendChild(dom)
 }
