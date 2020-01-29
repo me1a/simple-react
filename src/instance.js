df93f0a0590b66815e5be8aba172faa123f4dedd
me1a <mxh4373@gmail.com>
2020-01-29 22:15:43 +0800

    update property

--- src/instance.js
+++ src/instance.js
@@ -1,20 +1,10 @@
+import updateDomProperties from './updateDomProperties'
 
 function instantiate(element) {
     const { type, props } = element
     const isTextElement = type === 'TEXT ELEMENT'
     const dom = isTextElement ? document.createTextNode('') : document.createElement(type)
-
-    const isListener = name => name.startsWith('on')
-    const isAttribute = name => !isListener(name) && name !== 'children'
-
-    Object.keys(props).forEach(prop => {
-        if (isListener(prop)) {
-            const eventType = prop.toLowerCase().substring(2)
-            dom.addEventListener(eventType, props[prop])
-        } else if (isAttribute(prop)) {
-            dom[prop] = props[prop]
-        }
-    })
+    updateDomProperties(dom, {}, props)
 
     const childElements = props.children || []
     const childInstances = childElements.map(child => instantiate)
