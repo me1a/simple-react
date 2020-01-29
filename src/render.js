4e33229d81bc26b95f2159c689e7b815d40055ed
me1a <mxh4373@gmail.com>
2020-01-29 22:46:21 +0800

    render function change

--- src/render.js
+++ src/render.js
@@ -1,28 +1,11 @@
+import diff from './diff'
+
+let rootInstance = null
+
 function render(vNode, parentDom) {
-
-    const { type, props } = vNode
-    const isTextElement = type === 'TEXT ELEMENT'
-    const dom = isTextElement ? document.createTextNode('') : document.createElement(type)
-
-
-    const isListener = name => name.startsWith('on')
-    const isAttribute = name => !isListener(name) && name != "children"
-    Object.keys(props).forEach(prop => {
-        if (isListener(prop)) {
-            const eventType = prop.toLowerCase().substring(2)
-            dom.addEventListener(eventType, props[prop])
-        } else if (isAttribute(prop)) {
-            dom[prop] = props[prop]
-        } else {
-            // 递归将 vNode 转化为真实dom
-            const children = props.children || []
-            children.forEach(child => render(child, dom))
-        }
-    })
-
-
-
-    parentDom.appendChild(dom)
+    const prevInstance = rootInstance
+    const nextInstance = diff(parentDom, prevInstance, vNode)
+    rootInstance = nextInstance
 }
 
 export default render
